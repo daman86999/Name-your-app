2c3edf8b7c981e62d6724c63d6d401bee40b8686
daman86999 <52445744+daman86999@users.noreply.github.com>
2021-05-24 13:13:12 +0530

    header added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './../Header/Header';
 import './App.css';
 
 class App extends React.Component {
     
     render() { 
-        return <h1>Tjis is class</h1>;
+        return (
+            <div>
+                <Header/>
+            </div>
+        );
     }
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Header extends Component {
+    
+    render() { 
+        return ( 
+            <div className="head-container">
+                <h1>Name It!</h1>
+            </div>
+         );
+    }
+}
+ 
+export default Header;
